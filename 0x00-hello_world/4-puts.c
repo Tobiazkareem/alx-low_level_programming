3b3b3a638e31e7a7490d74cda4f420b382d1ade2
Tobiazkareem <Tobiazkareem@gmail.com>
2023-08-10 16:11:34 -0700

    hello puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*C program that prints exactly
+ * "programming is like building a multilingual puzzle
+ * using function puts()
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return 0;
+}
