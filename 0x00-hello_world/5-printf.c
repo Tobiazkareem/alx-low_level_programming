6a9b6e9d27fd092f1e181b5d1aeab2955cb22fde
Tobiazkareem <Tobiazkareem@gmail.com>
2023-08-10 16:49:27 -0700

    printf helps format and print text to output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Printf is used to format
+ * and print text to standard output
+ * allows the control of formatting by using format specifiers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char string[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", string);
+
+	return (0);
+
+}
