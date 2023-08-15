d17be3c16790dabc1146db324afd5980979511f5
Tobiazkareem <Tobiazkareem@gmail.com>
2023-08-15 08:44:15 -0700

    Created a header file to contain my functions prototype

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints
+ * _put_char function
+ *
+ * _put_char prototype function declare in
+ * the main.h header file of the current directory
+ *
+ * I am not allowed to use standard libary
+ * printf, puts etc is forbidden to use
+ *
+ * Return: On success 0.
+ */
+
+int main(void)
+{
+	/*call the function in main.h*/
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
