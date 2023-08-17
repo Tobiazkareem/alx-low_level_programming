8e56bce04da34fcc60a0da00d5291f3ec32a8884
Tobiazkareem <Tobiazkareem@gmail.com>
2023-08-17 05:09:09 -0700

    debugging code to check if integer is + or -

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,17 +3,14 @@
 /**
  * main - test  function that prints if integer is + or -
  * we are testing when given a case study of zero 0
- *
  * Return: 0
  */
 
 int main(void)
 {
 	int i;
-
 	/* assign 0 to i */
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
