415e046a7e20010573125d2d95396666caf70899
Tobiazkareem <Tobiazkareem@gmail.com>
2023-08-21 18:01:39 -0700

    swap the values of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -11,6 +11,15 @@ void swap_int(int *a, int *b)
 {
 	/* swap a value with b and vice versa */
 	/* using dereferencing */
-	*a = 42;
-	*b = 98;
+	/**
+	 * *a = 42;
+	 * *b = 98;
+	 * gave the right output but not the right way
+	 */
+
+	/* declare a var to store pointer a */
+	int store = *a;
+
+	*a = *b;
+	*b = store;
 }
