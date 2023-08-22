a4479b381cc191d93170dab95ce5b847b51e0630
Tobiazkareem <Tobiazkareem@gmail.com>
2023-08-21 20:00:56 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string character
+ * @s: string
+ *
+ */
+
+void rev_string(char *s)
+{
+	int length = 0, c = 0;
+	char store; /*store string |*/
+
+	while (s[length] != '\0')
+		length++;
+
+	/* print reverse */
+	for (c = length - 1; c >= length / 2; c--)
+		{
+			store = s[c];
+			s[c] = s[length - c - 1];
+			s[length - c - 1] = store;
+		}
+}
