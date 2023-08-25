8193bcfcd040637dfc4ad93123a9168e8f2955ac
Tobiazkareem <Tobiazkareem@gmail.com>
2023-08-24 19:55:36 -0700

    capitalize each word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_string -  capitalize string
+ * @str: string to capitalize
+ *
+ * Return: return string
+ */
+
+char *cap_string(char *str)
+{
+	int count = 0;
+
+	while (str[count])
+	{
+		while (!(str[count] >= 'a' && str[count] <= 'z'))
+			count++;
+
+		if (str[count - 1] == ' ' ||
+				str[count - 1] == ',' ||
+				str[count - 1] == '\t' ||
+				str[count - 1] == '\n' ||
+				str[count - 1] == ';' ||
+				str[count - 1] == '.' ||
+				str[count - 1] == '|' ||
+				str[count - 1] == '?' ||
+				str[count - 1] == '"' ||
+				str[count - 1] == ')' ||
+				str[count - 1] == '(' ||
+				str[count - 1] == '{' ||
+				str[count - 1] == '}' ||
+				count == 0)
+			str[count] -= 32;
+
+				count++;
+	}
+	return (0);
+}
