64637235e7a23e868f42c2b8e9ec2690fe43d6dc
Tobiazkareem <Tobiazkareem@gmail.com>
2023-08-30 16:15:21 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - factorial of an int
+ * @n: integer value
+ *
+ * Return: return -1 to indicate an error
+ */
+
+int factorial(int n)
+{
+	/* declare variable res */
+	int result = n;
+
+	if (n < 0)
+		return (-1); /* error */
+
+	else if (n >= 0 && n <=1)
+		return (1);
+
+	result *= factorial(n -1);
+
+	return (result);
+}
